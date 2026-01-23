1e586da1c1f31466951d4c288160cb326e38e96a
TigoV2 <102575@glr.nl>
2026-01-23 12:25:24 +0100

    i looked over required links again

--- pages/upload/uploadDonationsforWork.php
+++ pages/upload/uploadDonationsforWork.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once "../cinfo/config.php";
-require_once "../auth/session.inc.php";
-require_once "../auth/auth.inc.php";
+require_once "../../cinfo/config.php";
+require_once "../../auth/session.inc.php";
+require_once "../../auth/auth.inc.php";
 require_once "../functions/functions.php";
 
 requireLogin();
